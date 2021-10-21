92e005da51bbf87a4ee70b729765cccbeb59f2d3
svelezp <69641241+svelezp@users.noreply.github.com>
2021-10-21 13:05:52 -0500

    Delete MiUniversidad/academica directory

--- MiUniversidad/academica/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from academica.models import Carrera, Curso, Estudiante, Matricula
-
-# Register your models here.
-
-admin.site.register(Carrera)
-admin.site.register(Estudiante)
-admin.site.register(Curso)
-admin.site.register(Matricula)
