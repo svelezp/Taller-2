70ae20b6f5a6db48f88cddd94ddbb2f14572690b
svelezp <69641241+svelezp@users.noreply.github.com>
2021-10-21 13:38:47 -0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+
+def formularioContacto(request):
+    return render(request, "formularioContacto.html")
+# Create your views here.
+
+def contactar(request):
+    if request.method == "POST":
+        asunto = request.POST["txtAsunto"]
+        mensaje = request.POST["txtMensaje"] + " / Email: " + request.POST["txtEmail"]
+        email_desde = settings.EMAIL_HOST_USER
+        email_para = ["salomonvelezperez@gmail.com"]
+        send_mail(asunto, mensaje, email_desde, email_para, fail_silently=False)
+        return render(request, "contactoExitoso.html")
+    return render(request, "formularioContacto.html")
